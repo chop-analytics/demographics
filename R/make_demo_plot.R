b55986b590a0a80fd3bd05b2ea5ca433202a127f
Paul <WILDENHAIP@EMAIL.CHOP.EDU>
2018-04-10 14:48:50 -0400

    add add data argument to make_demo_plots

--- R/make_demo_plot.R
+++ R/make_demo_plot.R
@@ -2,13 +2,14 @@
 #'
 #' Helper function to make stratified plots by each demographic field.
 #'
-#' @param demo Dataframe with demographic fields to plot
+#' @param data Dataframe with demographics
+#' @param demo Demographic fields to plot
 #'
 #' @import ggplot2
 #' @import ggthemes
 
-make_demo_plot <- function(demo){
-  ggplot(data = filter(demo_pivot, demo_group == demo), aes(x = demo_value, y = METRIC)) +
+make_demo_plot <- function(data, demo){
+  ggplot(data = filter(data, demo_group == demo), aes(x = demo_value, y = METRIC)) +
     geom_bar(stat = "summary", fun.y = "mean", fill = "#50a0f0") +
     theme_few() +
     labs(x = demo, y = "AVERAGE", title = paste(METRIC, "stratified by", demo))
